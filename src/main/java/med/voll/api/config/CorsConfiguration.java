68fe3dae3c648f4d89ef2268430d913edcbc2c0c
vinipolitta <vinicius.politta1@gmail.com>
2024-07-21 20:19:00 -0300

    feat: creat conection db ussing migrations

---
+++ src/main/java/med/voll/api/config/CorsConfiguration.java
@@ -0,0 +1,14 @@
+package med.voll.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class CorsConfiguration {
+
+        public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
